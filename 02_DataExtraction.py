d50ea61119bba18618c2d111ae4781222c71a217
sokea0510 <sokea0510@gmail.com>
2023-04-28 17:25:15 +0900

    Last Updated

--- 02_DataExtraction.py
+++ 02_DataExtraction.py
@@ -17,17 +17,17 @@ vertical_acc = df['GyroZ_L']
 sk_up_peaks, _ = find_peaks(vertical_acc, distance=10, height=40)
 sk_down_peaks, _ = find_peaks(-vertical_acc, distance=10, height=40)
 sk_midSwing_peaks, _ = find_peaks(np.gradient(vertical_acc), distance=10, height=40)
-# # plot graph and peak of original data
-# plt.plot(vertical_acc, '-', color='black', label='Original')
-# plt.plot(sk_up_peaks, vertical_acc[sk_up_peaks],
-#          "x", color='red', label='Highest Peaks')
-# # plt.plot(sk_midSwing_peaks, vertical_acc[sk_midSwing_peaks],
-# #          "o", color='blue', label='Middle Peaks')
-# plt.plot(sk_down_peaks, vertical_acc[sk_down_peaks],
-#          "o", color='green', label='Lowest Peaks')
-# plt.legend()
-# plt.title('Original & Detection Peak')
-# plt.show()
+# plot graph and peak of original data
+plt.plot(vertical_acc, '-', color='black', label='Original')
+plt.plot(sk_up_peaks, vertical_acc[sk_up_peaks],
+         "x", color='red', label='Highest Peaks')
+# plt.plot(sk_midSwing_peaks, vertical_acc[sk_midSwing_peaks],
+#          "o", color='blue', label='Middle Peaks')
+plt.plot(sk_down_peaks, vertical_acc[sk_down_peaks],
+         "o", color='green', label='Lowest Peaks')
+plt.legend()
+plt.title('Original & Detection Peak')
+plt.show()
 
 
 ## Smooth and Filter the necessary featrues by gaussian_filter1d
@@ -43,6 +43,7 @@ CSF = sk.root_of_squared_maximum(
 print(CSF)
 sk_CSF_peaks, _ = find_peaks(CSF['CSF_L'], distance=10, height=50)
 sk_CSF = CSF['CSF_L']
+t = range(len(CSF["CSF_L"]))
 # Plot View Data from AccX, GyroZ, and Ang_Y
 fig, ax = plt.subplots(4, 1, figsize=(12, 8))
 ax[0].plot(CSF['AccX_L_Filtered'])
@@ -61,70 +62,51 @@ ax[3].plot(CSF['CSF_L'])
 ax[3].set_xlabel('Time (s)')
 ax[3].set_ylabel('Amplitude')
 ax[3].set_title('CSF_L')
+
+ax[0].plot(t, data['AccX_L_Filtered'],'-')
+ax[0].plot(t, data['AccY_L'],'-')
+ax[0].plot(t, data['AccZ_L'],'-')
+ax[0].legend(['AccX_L', 'AccY_L', 'AccZ_L'])
+ax[0].set_xlabel('Time (s)')
+ax[0].set_ylabel('Amplitude')
+ax[0].set_title('Acc_L')
+ax[1].plot(t, data['AngleX_L'],'-')
+ax[1].plot(t, data['AngleY_L_Filtered'],'-')
+ax[1].plot(t, data['AngleZ_L'],'-')
+ax[1].legend(['AngleX_L', 'AngleY_L', 'AngleZ_L'])
+ax[1].set_xlabel('Time (s)')
+ax[1].set_ylabel('Amplitude')
+ax[1].set_title('Angle_L')
+ax[2].plot(t, data['GyroX_L'],'-')
+ax[2].plot(t, data['GyroY_L'],'-')
+ax[2].plot(t, data['GyroZ_L_Filtered'],'-')
+ax[2].legend(['GyroX_L', 'GyroY_L', 'GyroZ_L'])
+ax[2].set_xlabel('Time (s)')
+ax[2].set_ylabel('Amplitude')
+ax[2].set_title('Gyro_L')
+
 plt.tight_layout()
+
 # plt.plot(df, '-', color='black', label='Original')
 plt.show()
 plt.plot(CSF['CSF_L'], '-', color='black', label='CSF_L')
 plt.plot(sk_CSF_peaks, sk_CSF[sk_CSF_peaks],
          "x", color='red', label='CSF Peaks')
 plt.show()
-# # _col_list_ = ['GyroX', 'GyroY', 'GyroZ', 'AngleX']
-# # data1 = sk.dataset_signal_bandpass_filtering(
-# #         dataset=df,
-# #         cols_=_col_list_)
-# # print(data)
-# start_time1 = datetime.now()
-# data = pd.DataFrame(data)
-# vertical_accF = data['GyroZ_L_Filtered']
-# sk_up_peaksF, _ = find_peaks(vertical_accF, distance=10, height=40)
-# sk_down_peaksF, _ = find_peaks(-vertical_accF, distance=10, height=40)
-# sk_midSwing_peaksF, _ = find_peaks(np.gradient(vertical_accF), distance=10, height=40)
-# # # plot graph and peak of data filtered
-# # plt.plot(vertical_acc, '-', color='black', label='Original')
-# # plt.plot(vertical_accF, '-', color='blue', label='Filtered')
-# # plt.plot(sk_up_peaksF, vertical_accF[sk_up_peaksF],
-# #          "x", color='red', label='Highest Peaks')
-# # # plt.plot(sk_midSwing_peaks, vertical_acc[sk_midSwing_peaks],
-# # #          "o", color='blue', label='Middle Peaks')
-# # plt.plot(sk_down_peaksF, vertical_accF[sk_down_peaksF],
-# #          "o", color='green', label='Lowest Peaks')
-# # plt.legend()
-# # plt.title('Detection Peak and Filtered')
-# # plt.show()
-
-
-# # Define the labels for each sample in the data based on the number of peaks
-# # Define the labels for the two classes (walking and non-walking)
-# walking_label = 100
-# non_walking_label = 0
-
-# data["labels"] = np.zeros(len(data))
-# # print(data.shape[1])
-# # print(data)
-# # print(sk_up_peaksF)
-# leight = 0
-# leight1 = 0
-# for i in range(len(sk_up_peaksF)-1):
-#     leight =(sk_up_peaksF[i+1] - sk_up_peaksF[i]) + leight
-# for i in range(len(sk_down_peaksF)-1):
-#     leight1 =(sk_down_peaksF[i+1] - sk_down_peaksF[i]) + leight1
-# len_peak = leight / len(sk_up_peaksF)
-# len_peak1 = leight1 / len(sk_down_peaksF)
-
-# print(f"Up: {len_peak} and Down: {len_peak1}")
-
-# for i in range(len(sk_up_peaksF)-1):
-#     if sk_up_peaksF[i+1] - sk_up_peaksF[i] <= len_peak1*2:
-#         df.iloc[sk_up_peaksF[i]:sk_up_peaksF[i+1], data.shape[1]-1:data.shape[1]] = walking_label
-# for i in range(len(sk_down_peaksF)-1):
-#     if sk_down_peaksF[i+1] - sk_down_peaksF[i] <= len_peak1*2:
-#         df.iloc[sk_down_peaksF[i]:sk_down_peaksF[i+1], data.shape[1]-1:data.shape[1]] = walking_label
-
-# end_time1 = datetime.now()
-# print('Duration: {}\n'.format(end_time1 - start_time1))
-# # plt.plot(vertical_acc, '-', color='black', label='Original')
+# _col_list_ = ['GyroX', 'GyroY', 'GyroZ', 'AngleX']
+# data1 = sk.dataset_signal_bandpass_filtering(
+#         dataset=df,
+#         cols_=_col_list_)
+# print(data)
+start_time1 = datetime.now()
+data = pd.DataFrame(data)
+vertical_accF = data['GyroZ_L_Filtered']
+sk_up_peaksF, _ = find_peaks(vertical_accF, distance=10, height=40)
+sk_down_peaksF, _ = find_peaks(-vertical_accF, distance=10, height=40)
+sk_midSwing_peaksF, _ = find_peaks(np.gradient(vertical_accF), distance=10, height=40)
+# # plot graph and peak of data filtered
+# plt.plot(vertical_acc, '-', color='black', label='Original')
 # plt.plot(vertical_accF, '-', color='blue', label='Filtered')
-# plt.plot(df["labels"], "-", color='red', label='Labeling')
 # plt.plot(sk_up_peaksF, vertical_accF[sk_up_peaksF],
 #          "x", color='red', label='Highest Peaks')
 # # plt.plot(sk_midSwing_peaks, vertical_acc[sk_midSwing_peaks],
@@ -132,11 +114,54 @@ plt.show()
 # plt.plot(sk_down_peaksF, vertical_accF[sk_down_peaksF],
 #          "o", color='green', label='Lowest Peaks')
 # plt.legend()
-# plt.title('Filtered, Detection Peak and Labelling')
+# plt.title('Detection Peak and Filtered')
 # plt.show()
+
+
+# Define the labels for each sample in the data based on the number of peaks
+# Define the labels for the two classes (walking and non-walking)
+walking_label = 100
+non_walking_label = 0
+
+data["labels"] = np.zeros(len(data))
+# print(data.shape[1])
 # print(data)
-# # df.to_csv("Datasets/Datalabelling.csv", columns=["AccX_Filtered", "AccY_Filtered", "AccZ_Filtered", "GyroX_Filtered", "GyroY_Filtered", "GyroZ_Filtered", "labels"])
-# df.to_csv("Datasets/Datalabelling.csv", columns=["GyroX_L_Filtered", "GyroY_L_Filtered", "GyroZ_L_Filtered", "labels"], index=False)
+# print(sk_up_peaksF)
+leight = 0
+leight1 = 0
+for i in range(len(sk_up_peaksF)-1):
+    leight =(sk_up_peaksF[i+1] - sk_up_peaksF[i]) + leight
+for i in range(len(sk_down_peaksF)-1):
+    leight1 =(sk_down_peaksF[i+1] - sk_down_peaksF[i]) + leight1
+len_peak = leight / len(sk_up_peaksF)
+len_peak1 = leight1 / len(sk_down_peaksF)
+
+print(f"Up: {len_peak} and Down: {len_peak1}")
+
+for i in range(len(sk_up_peaksF)-1):
+    if sk_up_peaksF[i+1] - sk_up_peaksF[i] <= len_peak1*2:
+        df.iloc[sk_up_peaksF[i]:sk_up_peaksF[i+1], data.shape[1]-1:data.shape[1]] = walking_label
+for i in range(len(sk_down_peaksF)-1):
+    if sk_down_peaksF[i+1] - sk_down_peaksF[i] <= len_peak1*2:
+        df.iloc[sk_down_peaksF[i]:sk_down_peaksF[i+1], data.shape[1]-1:data.shape[1]] = walking_label
+
+end_time1 = datetime.now()
+print('Duration: {}\n'.format(end_time1 - start_time1))
+# plt.plot(vertical_acc, '-', color='black', label='Original')
+plt.plot(vertical_accF, '-', color='blue', label='Filtered')
+plt.plot(df["labels"], "-", color='red', label='Labeling')
+plt.plot(sk_up_peaksF, vertical_accF[sk_up_peaksF],
+         "x", color='red', label='Highest Peaks')
+# plt.plot(sk_midSwing_peaks, vertical_acc[sk_midSwing_peaks],
+#          "o", color='blue', label='Middle Peaks')
+plt.plot(sk_down_peaksF, vertical_accF[sk_down_peaksF],
+         "o", color='green', label='Lowest Peaks')
+plt.legend()
+plt.title('Filtered, Detection Peak and Labelling')
+plt.show()
+print(data)
+# df.to_csv("Datasets/Datalabelling.csv", columns=["AccX_Filtered", "AccY_Filtered", "AccZ_Filtered", "GyroX_Filtered", "GyroY_Filtered", "GyroZ_Filtered", "labels"])
+df.to_csv("Datasets/Datalabelling.csv", columns=["GyroX_L_Filtered", "GyroY_L_Filtered", "GyroZ_L_Filtered", "labels"], index=False)
 
 
 
